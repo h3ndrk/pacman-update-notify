a946bab60e7fd8b2a32447624aeeee8636ed1bc6
Max Mustermann <max.mustermann7643@web.de>
2015-04-24 17:52:23 +0200

    Path bug fix, improved notification content

--- Makefile
+++ Makefile
@@ -29,7 +29,7 @@ INTERVAL=45
 
 all:
 
-invoke:
+test-service:
 	systemctl start pacman-update-notify.service
 
 pacman-update-notify.service: pacman-update-notify.service.template
--- pacman-update-notify.sh
+++ pacman-update-notify.sh
@@ -2,10 +2,15 @@
 
 export DISPLAY=:0
 
-# Query database and show notification if needed
 UPDATES=`/usr/bin/pacman -Qqu`
 UPDATES_RETURN=$?
 if [ $UPDATES_RETURN -eq 0 ]
 then
-	/usr/bin/notify-send "Updates available" "Packages: `/usr/bin/pacman -Qqu | while read pkg; do echo -n "$pkg, "; done | rev | cut -c 3- | rev`"
+	UPDATE_AMOUNT=`/usr/bin/pacman -Qqu | sed -n '$='`
+	
+	if [ UPDATE_AMOUNT -eq 1 ]; then
+		/usr/bin/notify-send "1 update available" "Package: `/usr/bin/pacman -Qqu | while read pkg; do echo -n "$pkg, "; done | rev | cut -c 3- | rev`"
+	else
+		/usr/bin/notify-send "$UPDATE_AMOUNT updates available" "Packages: `/usr/bin/pacman -Qqu | while read pkg; do echo -n "$pkg, "; done | rev | cut -c 3- | rev`"
+	fi
 fi
